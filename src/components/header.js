677eed3611c180ca35f97570271ca30fba9bbe11
Malcolm <mcconaghym@live.co.uk>
2019-06-17 20:32:19 +0100

    Added Header component, added application scss

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+import '../styles/header.scss';
+
+function Header() {
+  return (
+    <Fragment>
+      <div className="header">
+        <div className="header__text">
+          Hotel Results
+        </div>
+      </div>
+    </Fragment>
+  )
+};
+
+export default Header;
--- src/components/hotel_results.js
+++ src/components/hotel_results.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+
+import Header from './header';
 
 function HotelResults() {
+  const [hotelList, setHotelList] = useState(
+    [
+      {
+        "name": "hotelone",
+        "starRating": 5,
+        "facilities": ["car park", "pool"]
+      },
+      {
+        "name": "hoteltwo",
+        "starRating": 3,
+        "facilities": ["car park", "gym"]
+      },
+      {
+        "name": "hotelthree",
+        "starRating": 3,
+        "facilities": ["gym", "pool"]
+      }
+    ]
+  );
+
+
   return (
-    <div>Hello World</div>
+    <Fragment>
+      <Header />
+      { hotelList.map(hotel => {
+        return (
+          <div>{ hotel.name }</div>
+        );
+      })}
+    </Fragment>
   )
 };
 
